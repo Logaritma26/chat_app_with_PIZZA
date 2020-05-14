fd5bf24abe7022b5c382646cca6a31e7c9350cee
Erol <erolyildiz18it@gmail.com>
2020-05-14 23:00:59 +0200

    hellö

--- chat_app/app/src/main/java/com/example/chat_app/MainActivity.java
+++ chat_app/app/src/main/java/com/example/chat_app/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.chat_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        button = findViewById(R.id.button);
 
         //wubba lubba dub duuuuuub
     }
